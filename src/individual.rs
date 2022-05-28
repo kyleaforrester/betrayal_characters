b0bcac06f18f5cbf55bf6b1c56c27fdd1e8e9d43
Kyle Forrester <kforrester@manh.com>
2022-05-28 12:53:39 -0400

    Finish initial implementation

---
+++ src/individual.rs
@@ -0,0 +1,270 @@
+use std::cmp;
+use rand::prelude::SliceRandom;
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+pub struct Individual {
+    pub id: usize,
+    pub fitness: i32,
+    rank_score: i32,
+    avg_score: i32,
+    pub x: i32,
+    pub y: i32,
+    chars: HashMap<String, HashMap<String, (Vec<i32>, usize)>>,
+}
+
+impl Individual {
+    pub fn new(x: i32, y: i32, id: usize, rankings: &HashMap<String, Vec<String>>) -> Individual {
+        
+        let mut chars = HashMap::new();
+        for name in rankings.get("Might").unwrap().iter() {
+            let mut stats: HashMap<String, (Vec<i32>, usize)> = HashMap::new();
+            stats.insert("Might".to_string(), (vec![2, 2, 3, 4, 5, 6, 7, 8], 2));
+            stats.insert("Speed".to_string(), (vec![2, 2, 3, 4, 5, 6, 7, 8], 3));
+            stats.insert("Know".to_string(), (vec![2, 2, 3, 4, 5, 6, 7, 8], 3));
+            stats.insert("Sanity".to_string(), (vec![2, 2, 3, 4, 5, 6, 7, 8], 3));
+
+            chars.insert(name.to_string(), stats);
+        }
+
+        let mut ind = Individual {
+            id: id,
+            fitness: i32::MAX,
+            rank_score: i32::MAX,
+            avg_score: i32::MAX,
+            x: x,
+            y: y,
+            chars: chars,
+        };
+        ind.mutate();
+        ind.score(rankings);
+        ind
+    }
+
+    pub fn breed(parent_a: &Individual, parent_b: &Individual, x: i32, y: i32, id: usize, rankings: &HashMap<String, Vec<String>>) -> Individual {
+
+        let mut chars = HashMap::new();
+        for name in parent_a.chars.keys() {
+            let character = if rand::random() {
+                parent_a.chars.get(name).unwrap().clone()
+            } else {
+                parent_b.chars.get(name).unwrap().clone()
+            };
+
+            chars.insert(name.to_string(), character);
+        }
+
+        let mut ind = Individual {
+            id: id,
+            fitness: i32::MAX,
+            rank_score: i32::MAX,
+            avg_score: i32::MAX,
+            x: x,
+            y: y,
+            chars: chars,
+        };
+        ind.mutate();
+        ind.score(rankings);
+        ind
+    }
+
+    fn mutate(&mut self) {
+        let mut rng = rand::thread_rng();
+        for stats in self.chars.values_mut() {
+            let mut total: i32 = stats.values().map(|x| x.0.iter().sum::<i32>()).sum();
+
+            for tup in stats.values_mut() {
+                // Mutate the list of numbers
+                for i in 0..tup.0.len() {
+                    // Only mutate 10% of integers
+                    if rand::random::<f32>() > 0.1 {
+                        continue;
+                    }
+
+                    let mut possibilities = Vec::new();
+
+                    // Cannot change starting value
+                    let cond = i != tup.1;
+
+                    // Cannot increment to be larger than next number
+                    let inc_1 = i == 7 || (i < 7 && tup.0[i] < tup.0[i+1]);
+                    // Cannot increment to more than 2 from previous number
+                    let inc_2 = i == 0 || (i > 0 && tup.0[i] <= tup.0[i-1] + 1);
+                    // Total cannot be more than 155
+                    let inc_3 = total < 155;
+                    // Cannot be greater than 4 if lower half, or ever more than 8
+                    let inc_4 = if i <= 3 {
+                        tup.0[i] < 4
+                    } else {
+                        tup.0[i] < 8
+                    };
+
+                    // Cannot decrement to be less than previous number
+                    let dec_1 = i == 0 || (i > 0 && tup.0[i] > tup.0[i-1]);
+                    // Cannot decrement to be more than 2 less than next number
+                    let dec_2 = i == 7 || (i < 7 && tup.0[i] >= tup.0[i+1] - 1);
+                    // Total cannot be less than 145
+                    let dec_3 = total > 145;
+                    // Cannot be less than 5 if upper half, or ever less than 1
+                    let dec_4 = if i >= 4 {
+                        tup.0[i] > 5
+                    } else {
+                        tup.0[i] > 1
+                    };
+
+                    possibilities.push(0);
+                    // Can we increment
+                    if cond && inc_1 && inc_2 && inc_3 && inc_4 {
+                        possibilities.push(1);
+                    }
+                    // Can we decrement
+                    if cond && dec_1 && dec_2 && dec_3 && dec_4 {
+                        possibilities.push(-1);
+                    }
+
+                    let delta = possibilities.choose(&mut rng).unwrap();
+                    tup.0[i] += delta;
+                    total += delta;
+                }
+            }
+
+            // Mutate the indexes.
+            // Find a pair of attributes 
+            // See which pairs of [3, 4, 5] of each could work
+            // Then pick one.
+            // 20% chance of happening.
+            if rand::random::<f32>() < 0.2 {
+                let attrs: Vec<String>= ["Might", "Know", "Speed", "Sanity"].choose_multiple(&mut rng, 2).map(|x| x.to_string()).collect();
+                let mut possibilities = Vec::new();
+                for i in -1i32..2 {
+                    for j in -1i32..2 {
+                        let mut new_stats = stats.clone();
+                        if i >= 0 {
+                            new_stats.get_mut(&attrs[0]).unwrap().1 += i as usize;
+                        } else {
+                            new_stats.get_mut(&attrs[0]).unwrap().1 -= i.abs() as usize;
+                        }
+                        if j >= 0 {
+                            new_stats.get_mut(&attrs[1]).unwrap().1 += j as usize;
+                        } else {
+                            new_stats.get_mut(&attrs[1]).unwrap().1 -= j.abs() as usize;
+                        }
+
+                        if Individual::valid_indexes(new_stats) {
+                            possibilities.push((i, j));
+                        }
+                    }
+                }
+                let delta = possibilities.choose(&mut rng).unwrap();
+
+                if delta.0 >= 0 {
+                    stats.get_mut(&attrs[0]).unwrap().1 += delta.0 as usize;
+                } else {
+                    stats.get_mut(&attrs[0]).unwrap().1 -= delta.0.abs() as usize;
+                }
+
+                if delta.1 >= 0 {
+                    stats.get_mut(&attrs[1]).unwrap().1 += delta.1 as usize;
+                } else {
+                    stats.get_mut(&attrs[1]).unwrap().1 -= delta.1.abs() as usize;
+                }
+            }
+
+            // Mutate starting values
+            // Find a pair of starting values that can increment/decrement together
+            // Only has a small chance of happening.  20% chance of checking for this.
+            if rand::random::<f32>() < 0.2 {
+                let attrs: Vec<String>= ["Might", "Know", "Speed", "Sanity"].choose_multiple(&mut rng, 2).map(|x| x.to_string()).collect();
+                let dec_tup = stats.get(&attrs[0]).unwrap();
+                let inc_tup = stats.get(&attrs[1]).unwrap();
+
+                let dec_idx = dec_tup.1;
+                let inc_idx = inc_tup.1;
+
+                let can_dec = dec_tup.0[dec_tup.1] > 2
+                    // cant be less than previous number
+                    && dec_tup.0[dec_tup.1] > dec_tup.0[dec_tup.1 - 1]
+                    // cant be two less than next number
+                    && dec_tup.0[dec_tup.1] >= dec_tup.0[dec_tup.1 + 1] - 1
+                    // cant have an upper number be less than 5
+                    && (dec_tup.1 <= 3 || dec_tup.0[dec_tup.1] >= 6);
+
+                let can_inc = inc_tup.0[inc_tup.1] < 6
+                    // cant be greater than next number
+                    && inc_tup.0[inc_tup.1] < inc_tup.0[inc_tup.1 + 1]
+                    // cant be two greater than previous number
+                    && inc_tup.0[inc_tup.1] <= inc_tup.0[inc_tup.1 - 1] + 1
+                    // cant have a lesser number greater than 4
+                    && (inc_tup.1 == 4 || inc_tup.0[inc_tup.1] < 4);
+
+                if can_dec && can_inc {
+                    stats.get_mut(&attrs[0]).unwrap().0[dec_idx] -= 1;
+                    stats.get_mut(&attrs[1]).unwrap().0[inc_idx] += 1;
+                }
+            }
+        }
+    }
+
+    fn valid_indexes(stats: HashMap<String, (Vec<i32>, usize)>) -> bool {
+
+        // Indexes are all >= 2 and <= 4
+        if !stats.values().all(|x| x.1 >= 2 && x.1 <= 4) {
+            return false;
+        }
+
+        // Starting values sum to 15
+        if stats.values().map(|x| x.0[x.1]).sum::<i32>() != 15 {
+            //println!("\tFail due to sum {:?}", stats);
+            return false;
+        }
+
+        // Constitution is 10 or 11
+        let constitution: usize = stats.values().map(|x| x.1).sum();
+        if constitution < 10 || constitution > 11 {
+            //println!("\tFail due to constitution{:?}", stats);
+            return false;
+        }
+
+        //println!("\tTrue {:?}", stats);
+        true
+    }
+
+    fn score(&mut self, rankings: &HashMap<String, Vec<String>>) {
+        let mut rank_score = 0;
+        let mut names: Vec<String> = self.chars.keys().map(|x| x.to_string()).collect();
+        for tup in rankings.iter() {
+            names.sort_by_key(|x| 0-Individual::attr_score(self, x, tup.0));
+            
+            for name in names.iter() {
+                let true_idx = tup.1.iter().position(|x| x == name).unwrap();
+                let my_idx = names.iter().position(|x| x == name).unwrap();
+                rank_score += (true_idx as i32 - my_idx as i32).abs();
+            }
+        }
+
+        let mut avg_score = 0;
+        for attr in rankings.keys() {
+            let my_sum: i32= self.chars.values().map(|x| {
+                let tup = x.get(attr).unwrap();
+                tup.0[tup.1]
+            } ).sum();
+            let my_avg = my_sum as f32 / (self.chars.keys().count() as f32);
+            if attr == "Might" {
+                avg_score += ((my_avg - 3.0).abs() * 10.0) as i32;
+            } else {
+                avg_score += ((my_avg - 4.0).abs() * 10.0) as i32;
+            }
+        }
+
+        self.avg_score = avg_score;
+        self.rank_score = rank_score;
+        self.fitness = avg_score + rank_score;
+    }
+
+    fn attr_score(&self, name: &str, attr: &str) -> i32 {
+        let tup = self.chars.get(name).unwrap().get(attr).unwrap();
+
+        return tup.0.iter().sum::<i32>() + 4 * tup.1 as i32 + 4 * tup.0[tup.1];
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::fs;
-use std::io::{self, Read};
-use std::collections::HashMap;
+use std::cmp;
+use std::io::{self, Read, Write};
+use std::collections::{HashMap, HashSet};
+use individual::Individual;
+use rand::Rng;
+use rand::prelude::SliceRandom;
 
-const FILE = "rankings.txt";
+mod individual;
+
+const FILE: &str = "rankings.txt";
 
 struct Population {
     pop: Vec<Individual>,
@@ -11,69 +17,99 @@ struct Population {
     window: i32,
     pop_size: usize,
     rankings: HashMap<String, Vec<String>>,
+    counter: usize,
 }
 
 impl Population {
     fn new(rankings: HashMap<String, Vec<String>>) -> Population {
+        let mut rng = rand::thread_rng();
         let pop_size = 1000;
+        let mut counter = 0;
+        let x_axis = 1000;
+        let y_axis = 1000;
 
         let mut pop = Vec::new();
         for _i in 0..pop_size {
-            pop.push(Individual::new(&rankings));
+            let new_x = rng.gen_range(0..x_axis);
+            let new_y = rng.gen_range(0..y_axis);
+            pop.push(Individual::new(new_x, new_y, counter, &rankings));
+            counter += 1;
         }
 
         Population {
             pop: pop,
-            x_axis: 1000,
-            y_axis: 1000,
+            x_axis: x_axis,
+            y_axis: y_axis,
             window: 100,
             pop_size: pop_size,
             rankings: rankings,
+            counter: counter,
         }
     }
 
     fn run(&mut self, gens: usize) {
         for i in 0..gens {
-            println!("Starting generation {} of {}", i, gens);
+            println!("Starting generation {} of {}", i + 1, gens);
 
-            let (parent_a, parent_b) = self.select_parents();
+            let mut rng = &mut rand::thread_rng();
+            loop {
+                let x = rng.gen_range(0..self.x_axis);
+                let y = rng.gen_range(0..self.y_axis);
 
-            self.pop.push(Individual::new(&parent_a, &parent_b, &self.rankings));
-            self.pop.push(Individual::new(&parent_a, &parent_b, &self.rankings));
+                // Allow the grid of individuals to be borderless.  The edges are connected to each
+                // other.
+                let h_w = self.window/2;
+                let mut cands: Vec<Individual> = self.pop.iter().filter(|i| (i.x >= x - h_w && i.x <= x + h_w)
+                                                   || (x + h_w > self.x_axis && i.x <= (x + h_w) % self.x_axis)
+                                                   || (x - h_w < 0 && i.x >= self.x_axis + x - h_w))
+                    .filter(|i| (i.y >= y - h_w && i.y <= y + h_w)
+                            || (y + h_w > self.y_axis && i.y <= (y + h_w) % self.y_axis)
+                            || (y - h_w < 0 && i.y >= self.y_axis + y - h_w))
+                    .cloned().collect();
+
+                if cands.len() < 4 {
+                    continue;
+                }
+
+                // Randomly select 4 candidates
+                cands = cands.choose_multiple(&mut rng, 4).cloned().collect();
+
+                // Sort by fitness ascending
+                cands.sort_by_key(|x| x.fitness);
+
+                // Kill the two worst solutions
+                self.cull(cands.pop().unwrap().id);
+                self.cull(cands.pop().unwrap().id);
+
+                // Breed the two best
+                for _i in 0..2 {
+                    let new_x = rng.gen_range(cmp::max(0, self.x_axis - h_w)..cmp::min(self.x_axis + h_w, self.x_axis));
+                    let new_y = rng.gen_range(cmp::max(0, self.y_axis - h_w)..cmp::min(self.y_axis + h_w, self.y_axis));
+                    self.pop.push(Individual::breed(&cands[0], &cands[1], new_x, new_y, self.counter, &self.rankings));
+                    self.counter += 1;
+                }
+                break;
+            }
         }
     }
 
-    fn select_parents(&mut self) {
-
-        let candidates = loop {
-            let x = rand::thread_rng().gen_range(0..self.x_axis);
-            let y = rand::thread_rng().gen_range(0..self.y_axis);
-
-            // Allow the grid of individuals to be borderless.  The edges are connected to each
-            // other.
-            let window = self.window/2;
-            let cand_idxs: Vec<usize> = self.pop.iter().filter(|i| (i.x >= x - window && i.x <= x + window)
-                                               || (x + window > self.x_axis && i.x <= (x + window) % self.x_axis)
-                                               || (x - window < 0 && i.x >= self.x_axis + x - window))
-                .filter(|i| (i.y >= y - window && i.y <= y + window)
-                        || (y + window > self.y_axis && i.y <= (y + window) % self.y_axis)
-                        || (y - window < 0 && i.y >= self.y_axis + x - window))
-                .map(|i| i.id).collect();
-
+    fn cull(&mut self, id: usize) {
+        self.pop.remove(self.pop.iter().enumerate().find(|x| x.1.id == id).unwrap().0);
+    }
 
 
 }
 
 fn main() {
 
-    let buffer = fs::read_to_string(FILE).expect(format!("Could not read from FILE {}", FILE));
+    let buffer = fs::read_to_string(FILE).expect(format!("Could not read from FILE {}", FILE).as_str());
 
     let mut rankings: HashMap<String, Vec<String>> = HashMap::new();
 
     for chunk in buffer.split("\n\n").filter(|x| x.len() > 0) {
         let mut attribute = String::new();
         let mut ranking = Vec::new();
-        for line in chunk.split("\n").enumerate() {
+        for line in chunk.split("\n").filter(|x| x.len() > 0).enumerate() {
             if line.0 == 0 {
                 attribute = line.1.strip_suffix(":").unwrap().to_string();
             } else {
@@ -84,53 +120,83 @@ fn main() {
     }
 
     // Validate 4 categories present
-    assert!(rankings.keys().count == 4);
-    assert!(['Might', 'Speed', 'Know', 'Sanity'].iter().all(|x| rankings.contains_key(x)));
+    assert!(rankings.keys().count() == 4);
+    assert!(["Might", "Speed", "Know", "Sanity"].iter().all(|x| rankings.contains_key(*x)));
 
     // Validate all characters present in each category
     let set = HashSet::from_iter(rankings.values().nth(0).unwrap().iter());
-    assert!(rankings.values().map(|x| HashSet::from_iter(x.iter())).all(|x| x == set));
+    for v in rankings.values() {
+        println!("{:?}", v);
+    }
+    assert!(rankings.values().map(|x| HashSet::from_iter(x.iter())).all(|x: HashSet<&String>| x == set));
 
-    let mut pop = Population::new(rankings);
-    let mut choice = String:new();
+    let mut pop = Population::new(rankings.clone());
     loop {
+        let mut choice = String::new();
         let prompt = "\nChoose from the following options:\n\
             1) Generate new population\n\
             2) Load population from file\n\
             3) Save population to file\n\
             4) Save best individual to file\n\
             5) Run generations\n\
+            6) Print best individual\n\
+            7) Print population\n
             Choice: ";
 
-        print!(prompt);
+        print!("{}", prompt);
 
         io::stdin().read_line(&mut choice).expect("Could not read from stdin!");
 
-        match choice {
-            "1" => pop = Population::new(),
-            "2" => pop = load_population(),
+        match choice.trim() {
+            "1" => pop = Population::new(rankings.clone()),
+            "2" => pop = load_population(&rankings),
             "3" => save_population(&pop),
             "4" => save_individual(&pop),
             "5" => run_generations(&mut pop),
+            "6" => print_individual(&pop),
+            "7" => print_population(&pop),
             _ => println!("Not a valid answer! User responded: {}", choice),
         }
     }
 }
 
-fn run_generations(&mut pop: Vec<Individual>) {
+fn run_generations(pop: &mut Population) {
     let mut response = String::new();
     
     print!("How many generations to spawn? ");
+    io::stdout().flush().unwrap();
 
-    io::stdin().read_line(&mut gens).expect("Could not read from stdin!");
+    io::stdin().read_line(&mut response).expect("Could not read from stdin!");
 
     let gens: usize = match response.trim().parse() {
-        Some(num) => num,
-        None => {
-            println!("Not a recognized response: {}", response);
+        Ok(num) => num,
+        Err(e) => {
+            println!("Not a recognized response: {}. Error: {}", response, e);
             return
         },
     };
 
     pop.run(gens);
+}
+
+fn load_population(rankings: &HashMap<String, Vec<String>>) -> Population{
+    Population::new(rankings.clone())
+}
+
+fn save_population(pop: &Population) {
+    ()
+}
+
+fn save_individual(pop: &Population) {
+    ()
+}
+
+fn print_individual(pop: &Population) {
+    println!("{:?}", pop.pop.iter().min_by_key(|x| x.fitness));
+}
+
+fn print_population(pop: &Population) {
+    for i in pop.pop.iter() {
+        println!("{:?}", i);
+    }
 }
